a3a21a3675cc4c091f2c99f5715a5dc1144e0ea0
Derek Samms <derek.samms@gmail.com>
2021-10-06 14:50:32 -0700

    changed host to db instance

--- server/queries.js
+++ server/queries.js
@@ -4,7 +4,7 @@ const { Pool, Client } = require('pg')
 const pool = new Pool({
   user: 'derek',
   password: '',
-  host: 'localhost',
+  host: 'ec2-18-118-107-137.us-east-2.compute.amazonaws.com',
   database: 'reviews',
   port: 5432,
 })
